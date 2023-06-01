31f69240349f1b796f288a33bea012a24cea125a
sage2dope <popezamani@gmail.com>
2023-06-01 23:27:37 +0100

    pdf, docs parser

--- app.py
+++ app.py
@@ -92,27 +92,36 @@ def main():  # sourcery skip: extract-method, use-named-expression
 
     st.header("PDF GURU :books:")
 
-    pdf_docs, docx_docs = st.file_uploader(
+    pdf_docss = st.file_uploader(
             "Upload your PDFs,Docx here and click on 'Process'", accept_multiple_files=True)
     if st.button("Process"):
-            with st.spinner("Processing"):
+        with st.spinner("Processing"):
+
+            if pdf_docss:
                 # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-    
+                raw_text = get_pdf_text(pdf_docss)
+
                 # get docx text
-                raw_docs = get_docs_text(docx_docs)
+            
+                docx_docs = [file for file in pdf_docss if file.name.endswith(".docx")]
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text, raw_docs)
+                if docx_docs:
+                    text = get_docs_text(docx_docs)
+                    st.text_area("Extracted Text", value=text)
+                else:
+                    st.warning("No .docx files uploaded.")
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+            # get the text chunks
+            text_chunks = get_text_chunks(raw_text, text)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+            # create vector store
+            vectorstore = get_vectorstore(text_chunks)
 
-    
+            # create conversation chain
+            st.session_state.conversation = get_conversation_chain(
+                vectorstore)
+
+
     user_question = st.text_input("Ask any questions about your documents:")
     if user_question:
         handle_userinput(user_question)
@@ -123,7 +132,7 @@ def main():  # sourcery skip: extract-method, use-named-expression
             " 1. Upload a pdf file📄\n"
             "2. Ask a question about the document💬\n"
             "3. Get instant answers about your document\n")
-        
+
 
         #Sidebar Construction
         st.sidebar.markdown('______')
