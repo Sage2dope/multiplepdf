2255ca6fba479190cdd70756341c9f700f64edde
sage2dope <popezamani@gmail.com>
2023-06-04 12:38:45 +0100

    app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history'][::-1]
 
-    for message in st.session_state.chat_history:
+    for i, message in enumerate(st.session_state.chat_history):
         if message.is_user:
             st.write(user_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
