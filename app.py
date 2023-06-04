c9611ec96420d153d08b990d5f49512d2b1f0b53
sage2dope <popezamani@gmail.com>
2023-06-04 13:31:59 +0100

    bug i=fixx

--- app.py
+++ app.py
@@ -83,10 +83,9 @@ def handle_userinput(user_question):
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-
+            st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
 
 
 def main():  # sourcery skip: extract-method, use-named-expression
@@ -167,12 +166,12 @@ def main():  # sourcery skip: extract-method, use-named-expression
                 st.empty()
     
     #Display Input History 
-    for i, message in enumerate(reversed(st.session_state.chat_history)):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+    for message in reversed(st.session_state.chat_history):
+        if message.user_question:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
-        st.write("---")
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        st.write('----')
 
     #Clear Input History 
     st.session_state.chat_history = []
