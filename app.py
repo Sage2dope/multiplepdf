aadbfb94fc79e67c287790d90bc7241a98e9a722
sage2dope <popezamani@gmail.com>
2023-06-04 13:33:37 +0100

    bug fix

--- app.py
+++ app.py
@@ -75,17 +75,16 @@ def get_conversation_chain(vectorstore):
 
 
 def handle_userinput(user_question):
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
-
-    response = st.session_state.conversation({'question': user_question, 'chat_history': st.session_state.chat_history})
-    st.session_state.chat_history = response['chat_history']
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history'][::-1]
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():  # sourcery skip: extract-method, use-named-expression
