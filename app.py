c1daea5177dd242cb90dae1f2c9fbeac2bb85d26
sage2dope <popezamani@gmail.com>
2023-06-06 14:17:29 +0100

    saved changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
@@ -12,7 +13,35 @@ from faq import faq
 import tiktoken
 import docx2txt
 import openai
-from nav import navbar
+
+
+
+
+EXAMPLE_NO = 1
+
+def navbar(navsystem = 1):
+    if navsystem == 1:
+        selected = option_menu(
+                menu_title=None,  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+                orientation="horizontal",
+                styles={
+                    "container": {"padding": "0!important", "background-color": "#fafafa"},
+                    "icon": {"color": "orange", "font-size": "25px"},
+                    "nav-link": {
+                        "font-size": "25px",
+                        "text-align": "left",
+                        "margin": "0px",
+                        "--hover-color": "#eee",
+                    },
+                    "nav-link-selected": {"background-color": "green"},
+                },
+            )
+
+    return selected
 
 
 
