387a03a667c70148f8a394971a7e8dd0b58235e4
sage2dope <popezamani@gmail.com>
2023-06-18 02:50:32 +0100

    fix bug

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 from sidebar import sidebar
-import button
 import tiktoken
 import docx2txt
 import openai
@@ -115,7 +114,56 @@ def main():  # sourcery skip: extract-method, use-named-expression
        st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html= True)
     st.markdown("<h1 style= 'text-align: center:'>ASTODOC📓</h1>", unsafe_allow_html=True)
 
-    button
+    uploaded_files = st.file_uploader(
+        "Upload your Documents here and click on 'Process'",
+        accept_multiple_files=True
+    )
+
+    if st.button('Process'):
+          with st.spinner('Processing'):
+                raw_pdf_text = ""
+                raw_docx_text = ""
+                unsupported_files = []
+
+
+                for uploaded_file in uploaded_files:
+                    if uploaded_file.type == 'application/pdf':
+                        #process pdf file
+                        pdf_text = get_pdf_text([uploaded_file])
+                        raw_pdf_text += pdf_text
+                    elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        #Process docx files 
+                        docx_text = get_docx_text([uploaded_file])
+                        raw_docx_text += docx_text
+                    else:
+                        #Unsupported file type 
+                        unsupported_files.append(uploaded_file.name)
+
+                #Combine text from pdf and docx. files 
+                raw_text = raw_pdf_text + raw_docx_text
+
+                if unsupported_files:
+                    unsupported_files_str = ", ".join(unsupported_files)
+                    st.error(
+                        f"Sorry, the following file(s) cannot be processed: {unsupported_files_str}. Please upload another file."
+                    )
+                else:
+
+
+
+                    # get the text chunks
+                    text_chunks = get_text_chunks(raw_text)
+
+                    # create vector store
+                    vectorstore = get_vectorstore(text_chunks)
+
+                    if vectorstore is None:
+                        #Error occured during processing 
+                        return
+                    else:
+                        # create conversation chain
+                        st.session_state.conversation = get_conversation_chain(
+                            vectorstore)
 
 
     #Handling User Input 
