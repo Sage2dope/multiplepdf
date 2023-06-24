a534ed9415aef8c3296b8a83c4403d74cbdc903d
sage2dope <popezamani@gmail.com>
2023-06-24 03:21:05 +0100

    modulerize

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from utility import main
 from faq import faq
 import tiktoken
 import docx2txt
@@ -86,120 +87,7 @@ def handle_userinput(user_question):
 
 
 
-def main():  # sourcery skip: extract-method, use-named-expression
-    
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
-
-    with open('design.css') as source_des:
-       st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html= True)
-    st.markdown("<h1 style= 'text-align: center:'>ASTODOC📓</h1>", unsafe_allow_html=True)
-
-    uploaded_files = st.file_uploader(
-        "Upload your Documents here and click on 'Process'",
-        accept_multiple_files=True
-    )
-
-    if st.button('Process'):
-          with st.spinner('Processing'):
-                raw_pdf_text = ""
-                raw_docx_text = ""
-                unsupported_files = []
-
-
-                for uploaded_file in uploaded_files:
-                    if uploaded_file.type == 'application/pdf':
-                        #process pdf file
-                        pdf_text = get_pdf_text([uploaded_file])
-                        raw_pdf_text += pdf_text
-                    elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                        #Process docx files 
-                        docx_text = get_docx_text([uploaded_file])
-                        raw_docx_text += docx_text
-                    else:
-                        #Unsupported file type 
-                        unsupported_files.append(uploaded_file.name)
-
-                #Combine text from pdf and docx. files 
-                raw_text = raw_pdf_text + raw_docx_text
-
-                if unsupported_files:
-                    unsupported_files_str = ", ".join(unsupported_files)
-                    st.error(
-                        f"Sorry, the following file(s) cannot be processed: {unsupported_files_str}. Please upload another file."
-                    )
-                else:
-
-                        
-                            
-                    # get the text chunks
-                    text_chunks = get_text_chunks(raw_text)
-
-                    # create vector store
-                    vectorstore = get_vectorstore(text_chunks)
-
-                    if vectorstore is None:
-                        #Error occured during processing 
-                        return
-                    else:
-                        # create conversation chain
-                        st.session_state.conversation = get_conversation_chain(
-                            vectorstore)
-
-    
-    #Handling User Input 
-    with st.form(key="user_input_form"):
-        user_question = st.text_input("Ask any questions about your documents:")
-        submit_button = st.form_submit_button("Submit")
-
-        if submit_button:
-            if not user_question:
-                st.error("You have not entered a question. Please enter a question.")
-            else:
-                handle_userinput(user_question)
-                st.empty()
-    
-    #Display Input History 
-    for message in reversed(st.session_state.chat_history):
-        if message.is_user:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-        st.write('----')
-
-    #Clear Input History 
-    st.session_state.chat_history = []
-
-        
-
-    #Sidebar structure
-    with st.sidebar:
-        st.sidebar.title("How to use")
-        st.sidebar.write(
-            " 1. Upload files📄\n"
-            "2. Ask a question about the documents💬\n"
-            "3. Get instant answers about your documents\n")
-        
-
-        #Sidebar Construction
-        st.sidebar.markdown('______')
-        st.sidebar.markdown('# About')
-        st.sidebar.markdown(
-            'ASTODOC📜 allows you to ask questions about your documents'
-            'and get accurate answers with instant citations.')
-        st.sidebar.markdown(
-            'This tool is a work in progress.\n' 
-            'You can contribute to the project on [LinkedIn](https://www.linkedin.com/in/abdulkareemozovehe/) with your feedback and suggestions💡')
-        st.sidebar.markdown(
-            'Developed by [Abdulkareem Ozovehe®](https://www.linkedin.com/in/abdulkareemozovehe/)'
-        )
-        st.sidebar.markdown('© 2023')
-        st.sidebar.markdown('______')
-
-        faq()
-
+main()
 
 
 
